48aa894fa0ce4a90e384d9cdc058c87cbb8e1d02
Vaishnavi <palvaishnavi2024@gmail.com>
2025-11-02 12:10:39 +0530

    Clean repo - copied frontend and backend from old project

--- frontend/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-// frontend/src/lib/utils.ts
-
-// A simple helper to join class names conditionally
-export function cn(...classes: (string | undefined | false)[]) {
-  return classes.filter(Boolean).join(" ");
-}
